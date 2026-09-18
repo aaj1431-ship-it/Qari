ebd9e7989a1957afdf8090d74c0dc9c3070c321c
aaj1431-ship-it <aaj1431@gmail.com>
2026-09-18 08:40:11 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // يحفظ ملفات التطبيق والمكتبات ليعمل دون إنترنت
-const CACHE = "qari-v8";
+const CACHE = "qari-v11";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
